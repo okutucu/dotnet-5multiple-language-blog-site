6b2b23ec900f04496af61c4fd74ea9a5b9f0bbab
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-09-06 15:02:54 +0300

    have created a DateTime Extension Method for our Image Upload Processes

---
+++ ProgrammersBlog.Shared/Utilities/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Utilities.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string FullDateAndTimeStringWithUnderscore(this DateTime dateTime)
+        {
+
+            return $"{dateTime.Millisecond}_{dateTime.Second}_{dateTime.Minute}_{dateTime.Hour}.{dateTime.Day}_{dateTime.Month}_{dateTime.Year}";
+
+            /* 
+                 OğuzhanKutucu_587_5_38_12_3_10_2020.png
+                 KaanKutucu_601_5_38_12_3_10_2022.png
+             */
+        }
+    }
+}
