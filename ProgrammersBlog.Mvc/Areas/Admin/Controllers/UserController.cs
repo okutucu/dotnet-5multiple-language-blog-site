ba10056b5b2ba8ff1dcc3ad518c3aaa5966c3bfb
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-09-06 14:00:56 +0300

    We created UserController, User Index View and UserListDto Class

---
+++ ProgrammersBlog.Mvc/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Entities.Dtos;
+using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
+
+namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
+{
+
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UserController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<User> users = await _userManager.Users.ToListAsync();
+            return View(new UserListDto
+            {
+                Users = users,
+                ResultStatus =ResultStatus.Success
+            });
+        }
+    }
+}
